70ac2b335b42a7befadec34326671e2ce6b4e9ca
szafrank <krzysztof.szafranski@hypersocial.pl>
2016-04-19 15:37:24 +0200

    komentarz3

--- kalkulator.py
+++ kalkulator.py
@@ -1,2 +1,9 @@
 def kalkulator(a, b, operacja)
     pass
+
+def kolo(r):
+    return math.pi*r*r
+def prostokat(a,b):
+    return a*b
+def trojkat(a,b,c):
+    return math.sqrt((a+b+c)*(a+b-c)*(a-b+c)*(-a+b+c))/4
